898ec750cd193db7672df66ed9a0df3d62f47bd4
macarena rembado <macarembado12@gmail.com>
2023-11-02 19:19:22 -0300

    modificacion de colores de la bienvenida

--- myecommerce/src/components/bienvenida/bienvenida.jsx
+++ myecommerce/src/components/bienvenida/bienvenida.jsx
@@ -7,7 +7,7 @@ const Bienvenida = () => {
     <div className="bienvenida-container">
       <h1 className="bienvenida-text">Bienvenidos a Nuestra Tienda Online</h1>
       <Link to="/products">
-        <button className="btn btn-dark bienvenida-button">Ver Productos</button>
+        <button className="btn btn-outline-dark bienvenida-button">Ver Productos</button>
       </Link>
     </div>
   );
