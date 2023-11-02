815bb660d05fdd0e5207c0e5f6a0c05d370e2975
macarena rembado <macarembado12@gmail.com>
2023-11-02 17:35:44 -0300

    le agregue el spinner a la carga del detalle de producto

--- myecommerce/src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ myecommerce/src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,26 +1,29 @@
-import { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import ItemDetail from "../itemDetail/itemDetail";
+import Spinner from 'react-bootstrap/Spinner';
 
 const ItemDetailContainer = () => {
-    const { id } = useParams()
-    const [producto, setProducto] = useState()
+    const { id } = useParams();
+    const [producto, setProducto] = useState(null);
 
     useEffect(() => {
         fetch(`https://fakestoreapi.com/products/${id}`)
             .then(res => res.json())
             .then(json => {
-                setProducto(json)
-                console.log(json)
+                setProducto(json);
+                console.log(json);
             })
-            .catch((error) => console.error(error))
+            .catch(error => console.error(error));
     }, [id]);
 
     return (
-      <>
-         <ItemDetail producto={producto}/>
-      </>
+        producto ? ( 
+            <ItemDetail producto={producto} />
+        ) : (
+            <Spinner animation="border" variant="danger" />
+        )
     );
 };
 
-export default ItemDetailContainer
+export default ItemDetailContainer;
